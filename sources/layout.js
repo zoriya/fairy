51794258c58658af0673b2864fafa25373fbaa38
Zoe Roux <zoe.roux@zoriya.dev>
2023-05-11 17:56:24 +0900

    Add monocle layout

--- sources/layout.js
+++ sources/layout.js
@@ -6,24 +6,16 @@ var Layout = GObject.registerClass(
 	class Layout extends GObject.Object {
 		_init() {
 			super._init();
-			// /**
-			//  * Map<monitorIndex, Map<workspaceNbr - 0 for all, Window>>
-			//  * @type {Map<number, Map<number, FairyWindow[]>>}
-			//  */
-			// this._monitors = new Map();
-			//
-			// /**
-			//  * The
-			//  * @type {Map<number, number>}
-			//  */
-			// this._monitorsTags = new Map();
-
-			// TODO: duplicate this layout for each monitors.
-			this._layout = {
-				type: "tiled",
+			// Simpler to set 30 monitors than track creation/supression of monitors.
+			this._monitors = new Array(30).map(() => ({
+				/**
+				 * @type {Meta.Window} focused window's handle
+				 */
+				focused: null,
+				layout: "tiled",
 				nmaster: 1,
 				nfact: 60,
-			};
+			}));
 
 			/**
 			 * @type {FairyWindow[]}
@@ -46,7 +38,7 @@ var Layout = GObject.registerClass(
 				tags,
 				// Needed for the popByActor that might get called when the actor is already deleted
 				actor: handle.get_compositor_private(),
-			}
+			};
 		}
 
 		/**
@@ -61,8 +53,8 @@ var Layout = GObject.registerClass(
 		 * @returns {[FairyWindow, FairyWindow]} [old, new]
 		 */
 		updateByHandle(handle) {
-			const i = this.windows.findIndex(x => x.handle === handle);
-			const old = {...this.windows[i]};
+			const i = this.windows.findIndex((x) => x.handle === handle);
+			const old = { ...this.windows[i] };
 			this.windows[i] = this._windowFromHandle(handle);
 			return [old, this.windows[i]];
 		}
@@ -88,35 +80,48 @@ var Layout = GObject.registerClass(
 		 * @returns WindowGeometry[]
 		 */
 		render(mon, tags) {
-			const { nmaster, nfact } = this._layout;
+			const { layout, nmaster, nfact } = this._monitors[mon];
 			const windows = this.windows.filter(
 				(x) => x.monitor === mon && x.tags & tags
 			);
 			log(`${windows.length} windows for monitor ${mon} with tags ${tags}`);
 
 			// TODO: Implement other layouts
-			if (this._layout.type === "tiled") {
-				return windows.map((x, i) => {
-					const stackLength =
-						i < nmaster
-							? Math.min(nmaster, windows.length)
-							: windows.length - nmaster;
-					const stackIndex = i < nmaster ? i : i - nmaster;
-					return {
-						handle: x.handle,
-						maximized: false,
-						minimized: false,
-						x: i < nmaster ? 0 : nfact,
-						y: stackIndex * (100 / stackLength),
-						width:
-							windows.length <= nmaster
-								? 100
-								: i < nmaster
-									? nfact
-									: 100 - nfact,
-						height: 100 / stackLength,
-					};
-				});
+			switch (layout) {
+				case "monocle":
+					return [
+						{
+							handle: this._monitors[mon].focused,
+							maximized: true,
+							minimized: false,
+							x: 0,
+							y: 0,
+							width: 100,
+							height: 100,
+						},
+					];
+				case "tiled":
+					return windows.map((x, i) => {
+						const stackLength =
+							i < nmaster
+								? Math.min(nmaster, windows.length)
+								: windows.length - nmaster;
+						const stackIndex = i < nmaster ? i : i - nmaster;
+						return {
+							handle: x.handle,
+							maximized: false,
+							minimized: false,
+							x: i < nmaster ? 0 : nfact,
+							y: stackIndex * (100 / stackLength),
+							width:
+								windows.length <= nmaster
+									? 100
+									: i < nmaster
+										? nfact
+										: 100 - nfact,
+							height: 100 / stackLength,
+						};
+					});
 			}
 		}
 	}
